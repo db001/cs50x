d2b8d2cab3464431e0398774233921273c8950f9
David Bennett <ubuntu@dfc2eabdf236>
2017-01-03 21:13:21 +0000

    mario.c complete and passes test

---
+++ pset1/README.md
@@ -0,0 +1,3 @@
+# cs50x
+
+Repository of problem sets from edx's CS50x
--- pset1/mario.c
+++ pset1/mario.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main (void)
 {
     int height;
     do
     {
-        printf("How high is your pyramid? ");
+        printf("Height: ");
         height = get_int();
     } 
     while (23 < height || height < 0);
     
+    if (height > 0)
+    {
     
-    printf("Height: %i\n", height);
+        for (int i = 1; i <= height; i++)
+        {
+        
+            for (int x = height - i; x > 0; x--)
+            {
+                printf(" ");
+            }
+        
+            for (int y = 1; y <= i; y++)
+            {
+                printf("#");
+            }
+            
+            printf("  ");
+            
+            for (int z = 1; z <= i; z++)
+            {
+                printf("#");
+            }
+        
+            printf("\n");
+        
+        }
+    
+    }
+    
+    return 0;
+    
 }
