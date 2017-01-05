c8e2d286f8d9c8c305b347cc74e0ac978d403a35
David Bennett <ubuntu@dfc2eabdf236>
2017-01-05 16:34:49 +0000

    credit.c first 2 numbers shown and validated

--- pset1/credit.c
+++ pset1/credit.c
@@ -12,11 +12,14 @@ int main (void)
     int count = 0;
     string validNum = "INVALID";
     
-    // Example card num: 378282246310005 (AmEx);
+    // Example card num:
+    // 378282246310005 (AmEx);
+    // 4012888888881881 (VISA);
+    // 5105105105105100 (MCard);
     
-    // American Express numbers all start with 34 or 37 && 15 digits long;
-    // MasterCard numbers all start with 51, 52, 53, 54, or 55 && 16 digits long;
-    // Visa numbers all start with 4 && 13 or 16 digits long;
+    // American Express numbers all start with 34 or 37 && are 15 digits long;
+    // MasterCard numbers all start with 51, 52, 53, 54, or 55 && are 16 digits long;
+    // Visa numbers all start with 4 && are 13 or 16 digits long;
     
     // Get card number and prompt if negative number is given
     do
@@ -26,7 +29,7 @@ int main (void)
     }
     while (cardNum <= 0);
     
-    int num = cardNum;
+    long long num = cardNum;
 
     // Go through card number and apply logic to individual digits
     while (num > 0)
@@ -56,7 +59,7 @@ int main (void)
     int countMinus = count - 2;
     long long first = cardNum / (pow(10, countMinus));
     
-    // printf("Total: %i\nCount - 2: %i\nValid Number: %s\n", total, countMinus, validNum);
+    printf("Total: %i\nCount: %i\nValid Number: %s\n", total, count, validNum);
     printf("First: %lld\n", first);
     
     return 0;
