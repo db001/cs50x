d46ba133121f892231bec4529823228b8eff356a
David Bennett <ubuntu@dfc2eabdf236>
2017-01-05 13:40:29 +0000

    credit.c number validation done

---
+++ pset1/credit.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main (void) 
+{
+    
+    long long cardNum;
+    int doubles = 0;
+    int otherDigits = 0;
+    int total;
+    int count = 0;
+    string validNum = "INVALID";
+    
+    // Example card num: 378282246310005 (AmEx);
+    
+    // American Express numbers all start with 34 or 37 && 15 digits long;
+    // MasterCard numbers all start with 51, 52, 53, 54, or 55 && 16 digits long;
+    // Visa numbers all start with 4 && 13 or 16 digits long;
+    
+    // Get card number and prompt if negative number is given
+    do
+    {
+        printf("Number: ");
+        cardNum = get_long_long();
+    }
+    while (cardNum <= 0);
+    
+    int num = cardNum;
+
+    // Go through card number and apply logic to individual digits
+    while (num > 0)
+    {
+      if (count % 2 == 1) {
+          int x = (num % 10) * 2;
+          while (x > 0) {
+              doubles += x % 10;
+              x /= 10;
+          }
+          
+      } else {
+          otherDigits += num % 10;
+      }
+      num /= 10;
+      count += 1;
+    }
+    
+    // Check if last digit of total = 0 to show a valid card number
+    total = doubles + otherDigits;
+    
+    if (total % 10 == 0) {
+        validNum = "VALID";
+    }
+    
+    // Get first 2 digits of number
+    int countMinus = count - 2;
+    long long first = cardNum / (pow(10, countMinus));
+    
+    // printf("Total: %i\nCount - 2: %i\nValid Number: %s\n", total, countMinus, validNum);
+    printf("First: %lld\n", first);
+    
+    return 0;
+}
