b111c12ed9cccd5418f82a71ac8bb5104080d96d
David Bennett <ubuntu@dfc2eabdf236>
2017-01-15 15:32:52 +0000

    caesar started

--- pset2/initials.c
+++ pset2/initials.c
@@ -1,17 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
     string s = get_string();
     if (s != NULL)
     {
+        if (s[0] != 32) {
+            printf("%c", toupper(s[0]));
+        }
         for (int i = 0, n = strlen(s); i < n; i++)
         {
-            printf("%c\n", s[i]);
+            if (i < n - 1 && s[i] == 32 && s[i + 1] != 32) {
+                printf("%c", toupper(s[i + 1]));
+            }
+            
         }
-        printf("%s\n", s);
+        printf("\n");
         return 0;
         
     } else {
