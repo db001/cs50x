3c8faf235811eab1383f5b4fe03541833098000f
David Bennett <ubuntu@dfc2eabdf236>
2017-01-17 13:38:41 +0000

    vigenere failing 2 checks

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -21,22 +21,31 @@ int main (int argc, string argv[])
     
     string cipher = argv[1];
     int x = strlen(cipher);
-    
+
     printf("ciphertext: ");
+    
+    int count = 0;
     
     for (int i = 0, n = strlen(text); i < n; i++) {
         
+        int c = text[i] + cipher[count % x] - 97; // Needs to be an int as char overflows at more than 127
+        
         if (isalpha(text[i])) {
-            char c = text[i] + cipher[i % x] - 97;
-            if (c - 97 > 26) {
+            
+            if (islower(text[i]) && c > 122) {
+                printf("%c", c - 26);
+            } else if (isupper(text[i]) && c > 90) {
                 printf("%c", c - 26);
             } else {
                 printf("%c", c);
             }
+            
+            count += 1;
+            
         } else {
             printf("%c", text[i]);
         }
-        
+         
     }
     
     printf("\n");
